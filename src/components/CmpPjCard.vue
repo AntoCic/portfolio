7ecab48a3d848ae795ffbfdfeecf59ee93ff36b2
AntoCic <anto.cic.127@gmail.com>
2024-06-25 22:32:18 +0200

    ADD style to home

---
+++ src/components/CmpPjCard.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="ms_card">
+
+        <img :src="pj.img" class="logo-pj" alt="logo-project">
+        <div class="overlai"></div>
+        <div class="ms_content p-3">
+            <h5 class="card-title">{{ pj.name }}</h5>
+            <p class="card-text small">{{ pj.rule }}</p>
+            <div class="mb-2">
+                <img v-for="technology in pj.technologies" :src="`/img/skills/${technology.toLowerCase()}.svg`" :alt="technology" width="25" class="me-1">
+                
+            </div>
+            <p class="card-text">{{ pj.description }}</p>
+            <p>Data: {{ pj.dateStart }} <template v-if="pj.dateEnd">- {{ pj.dateEnd }}</template> </p>
+            <div class="mt-auto">
+                <a v-if="pj.site_link" :href="pj.site_link" target="_blank" class="btn btn-link">Stite To
+                    Try</a>
+                <a v-if="pj.git_link" :href="pj.git_link" target="_blank" class="btn btn-link">Github</a>
+            </div>
+        </div>
+
+    </div>
+    <!-- <div class="card mb-3 text-azul bg-light bg-opacity-10 border-0 rounded">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img :src="pj.img" class="object-fit-cover  rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">{{ pj.name }}</h5>
+                    <p class="card-text small">{{ pj.rule }}</p>
+                    <div class="mb-2">
+                        <span v-for="technology in pj.technologies" class="badge text-bg-primary me-2">{{ technology
+                            }}</span>
+                    </div>
+                    <p class="card-text">{{ pj.description }}</p>
+                    <p>Data: {{ pj.dateStart }} <template v-if="pj.dateEnd">- {{ pj.dateEnd }}</template> </p>
+<div class="mt-auto">
+    <a v-if="pj.site_link" :href="pj.site_link" target="_blank" class="btn btn-link">Stite To
+        Try</a>
+    <a v-if="pj.git_link" :href="pj.git_link" target="_blank" class="btn btn-link">Github</a>
+</div>
+</div>
+</div>
+</div>
+</div> -->
+</template>
+
+<script>
+export default {
+    props: {
+        pj: {
+            type: Object,
+            required: true
+        }
+    },
+}
+</script>
+
+
+<style lang="scss" scoped>
+.ms_card {
+    position: relative;
+    width: 100%;
+    display: flex;
+
+    .logo-pj {
+        width: 40%;
+        object-fit: cover;
+        object-position: left;
+    }
+
+    .overlai {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(90deg, rgba(28, 64, 93, 0) 10%, rgba(28, 64, 93, 1) 40%);
+    }
+
+    .ms_content {
+        width: 60%;
+        position: relative;
+        z-index: 1;
+    }
+}
+</style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,76 +1,59 @@
 <template>
-  <div class="text-azul">
-    <h1>{{ store.user.name }}</h1>
-    <p class="small">{{ store.user.rule }}</p>
-    <p>{{ store.user.description }}</p>
-    <hr>
-    <h2>SKILLS</h2>
-    <div class="mb-2">
-      <span v-for="technology in store.user.technologies" class="badge text-bg-primary me-2">{{ technology }}</span>
-    </div>
+  <div class="row text-azul">
 
-    <hr>
-
-    <h2>Project</h2>
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
-
-      <div class="col py-3" v-for="(pj, key) in store.visiblePj" :key="key">
-        <div class="card p-2 text-azul bg-light bg-opacity-10 border-0 rounded h-100">
-          <img :src="pj.img" class="card-img-top" alt="logo-progetto">
-          <div class="card-body">
-            <h5 class="card-title">{{ pj.name }}</h5>
-            <p class="card-text small">{{ pj.rule }}</p>
-            <div class="mb-2">
-              <span v-for="technology in pj.technologies" class="badge text-bg-primary me-2">{{ technology }}</span>
-            </div>
-            <p class="card-text">{{ pj.description }}</p>
-            <p>Data: {{ pj.dateStart }} <template v-if="pj.dateEnd">- {{ pj.dateEnd }}</template> </p>
-            <div class="mt-auto">
-              <a v-if="pj.site_link" :href="pj.site_link" target="_blank" class="btn btn-link">Stite To Try</a>
-              <a v-if="pj.git_link" :href="pj.git_link" target="_blank" class="btn btn-link">Github</a>
-            </div>
-
+    <div class="col-12 col-md-4 col-lg-3">
+      <h2>{{ store.user.name }}</h2>
+      <p class="small">{{ store.user.rule }}</p>
+      <p>{{ store.user.description }}</p>
+      <hr>
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h4>SKILLS</h4>
+          </div>
+          <div class="col-3 col-sm-4 col-md-12" v-for="technology in store.user.technologies">
+            <p class="text-center text-sm-start"><img :src="`/img/skills/${technology.toLowerCase()}.svg`" :alt="technology" class="me-1"> {{ technology
+              }}</p>
           </div>
         </div>
       </div>
+
+
+
+
     </div>
 
+    <div class="col-12 col-md-8 col-lg-9">
+      <h3 class="text-end">Project</h3>
+      <hr class="mt-0">
 
-    <!-- <div v-for="(pj, key) in store.visiblePj" :key="key"
-      class="card text-azul bg-light bg-opacity-10 border-0 rounded mb-2">
-      <div class="card-body">
-        <h5 class="card-title">{{ pj.name }}</h5>
-        <p class="card-text small">{{ pj.rule }}</p>
-        <div class="mb-2">
-          <span v-for="technology in pj.technologies" class="badge text-bg-primary me-2">{{ technology }}</span>
+      <CmpPjCard v-for="(pj, key) in store.visiblePj" :key="key" :pj="pj" />
+
+    </div>
+
+    <div class="col-12 text-azul">
+      <hr>
+      <h2>Experience</h2>
+      <div v-for="(experience, key) in store.experiences" :key="key"
+        class="card text-azul bg-light bg-opacity-10 border-0 rounded mb-2">
+        <div class="card-body">
+          <h5 class="card-title">{{ experience.name }}</h5>
+          <p class="card-text small">{{ experience.rule }}</p>
+          <p class="card-text">{{ experience.description }}</p>
+          <p>Data: {{ experience.dateStart }} - {{ experience.dateEnd }}</p>
+          <p>Location: {{ experience.location }}</p>
         </div>
-        <p class="card-text">{{ pj.description }}</p>
-        <p>Data: {{ pj.dateStart }} - {{ pj.dateEnd }}</p>
-        <p>Location: {{ pj.location }}</p>
-        <img :src="pj.img" width="100" height="100" alt="">
-        <a v-if="pj.site_link" :href="pj.site_link" target="_blank" class="btn btn-link">Stite To Try</a>
-        <a v-if="pj.git_link" :href="pj.git_link" target="_blank" class="btn btn-link">Github</a>
-      </div>
-    </div> -->
-
-    <hr>
-
-    <h2>Experience</h2>
-    <div v-for="(experience, key) in store.experiences" :key="key"
-      class="card text-azul bg-light bg-opacity-10 border-0 rounded mb-2">
-      <div class="card-body">
-        <h5 class="card-title">{{ experience.name }}</h5>
-        <p class="card-text small">{{ experience.rule }}</p>
-        <p class="card-text">{{ experience.description }}</p>
-        <p>Data: {{ experience.dateStart }} - {{ experience.dateEnd }}</p>
-        <p>Location: {{ experience.location }}</p>
       </div>
     </div>
+
   </div>
 </template>
+
 <script>
 import { store } from '../store.js';
+import CmpPjCard from '../components/CmpPjCard.vue';
 export default {
+  components: { CmpPjCard },
   data() {
     return {
       store,
